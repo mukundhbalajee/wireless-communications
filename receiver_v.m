a0b3acd944e3a0ba88816724c678386b1eb0900b
Vikram Marmer <65619395+vtmarmer@users.noreply.github.com>
2023-11-12 17:56:35 -0500

    Transceiver works for BPSK
    Split 1440 bits into 4 segments. 
    0 BER!

--- receiver_v.m
+++ receiver_v.m
@@ -2,25 +2,26 @@ clear; close all; clc;
 load freq_sync;
 load t_sync.mat;
 load frame_sync.mat;
-load transmitsignal_test_cases.mat;
-load receivedsignal_test_cases.mat;
+load receivedsignal.mat;
+load transmitsignal.mat;
+load bits.mat
 sync_len = 1; % microseconds
 fs = 200; %MHz
 upsampling_rate = 20;
-pulse = rcosdesign(0.5, 7, upsampling_rate, 'sqrt');
+pulse = rcosdesign(0.9, 30, upsampling_rate, 'sqrt');
 
 preamble = horzcat(freq_sync, t_sync, frame_sync);
 impulse = horzcat(zeros(1, 100), 1, zeros(1, 100));
 sine = horzcat(zeros(1, 100), sin(2*pi*5e6.*linspace(0, 50e-6, 500)), zeros(1, 100));
 square = horzcat(zeros(1, 100), ones(1, 100), zeros(1, 100));
 
-%% Timing Synchronization
-
-%Demodulate
+%% Demodulate
 wt = fliplr(pulse); %probably unnecessary
 zt = conv(wt, receivedsignal);
 
-sig_to_find = t_sync;  %PUT SIGNAL TO FIND HERE
+%% Timing Synchronization
+
+sig_to_find = preamble;  %PUT SIGNAL TO FIND HERE
 
 xt_conj = conj(sig_to_find); 
 
@@ -28,61 +29,119 @@ tau = length(zt);
 sums = zeros(1, tau);
 p = length(xt_conj);
 
-for i = 1:tau-p*upsampling_rate-1
+for i = 1:tau-p*upsampling_rate
     sums(i) = 0;
-    for k = 1:p
-        sums(i) = sums(i) + xt_conj(k)*zt(k*upsampling_rate+i);
+    for k = 0:p-1
+        sums(i) = sums(i) + xt_conj(k+1)*zt(k*upsampling_rate+i);
     end
 end
 
-timing_offset = find(sums == max(sums));
-zt = zt(timing_offset:end);
+timing_offset = find(abs(sums) == max(abs(sums)));
+zt_timing = zt(timing_offset+upsampling_rate:end);
 phase = angle(max(sums));
-zt_inphase_timing = zt .* exp(-j*phase); % Remove phase offset
+zt_inphase_timing = zt_timing .* exp(-j*phase); % Remove phase offset
 
 %% Frame Synchronization
 
 sig_to_find = frame_sync;  %PUT SIGNAL TO FIND HERE
-
+num_frames = 4;
 xt_conj = conj(sig_to_find);
 
-tau = length(zt);
+tau = length(zt_inphase_timing);
 sums = zeros(1, tau);
 p = length(xt_conj);
 
 for i = 1:tau-p*upsampling_rate-1
     sums(i) = 0;
     for k = 1:p
-        sums(i) = sums(i) + xt_conj(k)*zt(k*upsampling_rate+i);
+        sums(i) = sums(i) + xt_conj(k)*zt_inphase_timing(k*upsampling_rate+i);
     end
 end
 
-offset = find(sums == max(sums));
-phase = angle(max(sums));
-
-zt_inphase_frame = zt .* exp(-j*phase); % Remove phase offset
-
-start_ind = offset + length(frame_sync);
-zt_inphase_frame = zt_inphase_frame(start_ind:end);
+[pks,locs] = findpeaks(abs(sums), 'MinPeakProminence', 10);
+phases = zeros(1, num_frames);
+starts = zeros(1, num_frames);
+for i = 1:4
+   phases(i) = angle(sums(locs(i)));
+   starts(i) = locs(i) + upsampling_rate*(length(frame_sync)+1);
+end
 
 %% Equalization
-received_preamble = zt_inphase_frame(start_ind:start_ind+length(preamble)-1);
-channel_effect = received_preamble./preamble;
-h0=mean(channel_effect);
-% h0=0.2993;
-zt_inphase_frame = zt_inphase_frame./h0;
+received_preamble = zt_inphase_timing(1:upsampling_rate:length(preamble)*upsampling_rate);
+channel_effect = abs(received_preamble)'./abs(preamble);
+h0=mean(abs(channel_effect));
+zt_inphase_timing = zt_inphase_timing./h0;
 
-%% Plotting Results
-xt = zt_inphase_frame;
-len = length(xt);
-% Plot time domain signal
+%% Sampling
+% zk = zt_inphase_frame(1:upsampling_rate:length(zt_inphase_frame));
+frame1 = zt_inphase_timing(starts(1): starts(2)-upsampling_rate*(length(frame_sync))).* exp(-j*phases(1));
+frame2 = zt_inphase_timing(starts(2): starts(3)-upsampling_rate*(length(frame_sync))).* exp(-j*phases(2));
+frame3 = zt_inphase_timing(starts(3): starts(4)-upsampling_rate*(length(frame_sync))).* exp(-j*phases(3));
+frame4 = zt_inphase_timing(starts(4): end-1-upsampling_rate*(length(frame_sync))).* exp(-j*phases(4));
+
+zk1 = frame1(1:upsampling_rate:length(frame1));
+zk2 = frame2(1:upsampling_rate:length(frame2));
+zk3 = frame3(1:upsampling_rate:length(frame3));
+zk4 = frame4(1:upsampling_rate:length(frame4));
+
+% Guessing
+rx_ipts1 = sign(real(zk1));
+rx_ipts2 = sign(real(zk2));
+rx_ipts3 = sign(real(zk3));
+rx_ipts4 = sign(real(zk4));
+
+%Error checking and BER calculation
+data = imread('images/shannon1440.bmp');
+dims = size(data);
+bits = reshape(data, 1, dims(1)*dims(2));
+bits = bits .* 2 -1; %BPSK
+data1 = bits(1:400);
+data2 = bits(401:800);
+data3 = bits(801:1200);
+data4 = bits(1201:min(length(bits), 1600));
+
+BER1 = length(find(rx_ipts1(1:400) ~= data1'))/length(data1)
+BER2 = length(find(rx_ipts2(1:400) ~= data2'))/length(data2)
+BER3 = length(find(rx_ipts3(1:400) ~= data3'))/length(data3)
+BER4 = length(find(rx_ipts4(1:240) ~= data4'))/length(data4)
+
+%% Plotting
+
+figure; imshow(data); title("original")
+
+received_image = [rx_ipts1(1:400); rx_ipts2(1:400); rx_ipts3(1:400); rx_ipts4(1:240)];
+received_image = reshape(received_image, 45, 32);
+figure; imshow(received_image); 
+
+
+% xt = upsample(data4, upsampling_rate);
+% len = length(xt);
+% 
+% figure(1)
+% LargeFigure(gcf, 0.15); % Make figure large
+% clf
+% 
+% % Plot transmit x(t)
 % subplot(2,1,1);
-hold on;
-plot([0:len-1]/200e6*1e6, real(xt))
-plot([0:len-1]/200e6*1e6, imag(xt))
-hold off;
-xlabel('t in microseconds')
-ylabel('x(t)')
-legend("real", "imag");
-title('xbase(t)')
-axis tight
+% hold on;
+% plot([0:len-1], real(xt))
+% hold off;
+% xlabel('t in microseconds')
+% ylabel('f(t)')
+% title('xbase(t)')
+% axis tight
+% 
+% xt = zk4;
+% len = length(xt);
+% 
+% % Plot received signal of choice
+% subplot(2,1,2);
+% hold on
+% plot([0:len-1], real(xt))
+% plot([0:len-1], imag(xt))
+% hold off;
+% title('z(t) frame in phase')
+% xlabel('t in microseconds')
+% ylabel('x(t)')
+% legend("real", "imag");
+% axis tight
--- transmitter_v.m
+++ transmitter_v.m
@@ -1,33 +1,42 @@
 %% Setup
 clear; clc; close all;
 
-sync_len = 1; % microseconds
+sync_len = 5; % microseconds
 fs = 200; %MHz
 upsampling_rate = 20;
 
 % freq_sync = ones(1, fs*sync_len/upsampling_rate); %2 us of frame sync for the receiver to get PLL lock
 % t_sync = floor(2.*rand(1, fs*sync_len/upsampling_rate)).* 2 -1; %Pseudo-random BPSK
 % frame_sync = floor(2.*rand(1, fs*sync_len/upsampling_rate)).* 2 -1; %Pseudo-random BPSK
+% save("frame_sync", "frame_sync")
+% save("t_sync", "t_sync")
+% save("freq_sync", "freq_sync")
+
 load freq_sync;
 load t_sync.mat;
 load frame_sync.mat;
 
 preamble = horzcat(freq_sync, t_sync, frame_sync);
-impulse = horzcat(zeros(1, 100), 1, zeros(1, 100));
-sine = horzcat(zeros(1, 100), sin(2*pi*5e6.*linspace(0, 50e-6, 500)), zeros(1, 100));
-square = horzcat(zeros(1, 100), ones(1, 100), zeros(1, 100));
-test_sig = horzcat(preamble, impulse, zeros(1,40), sine, zeros(1, 40), square);
-test_sig = upsample(test_sig, upsampling_rate);
+% impulse = horzcat(zeros(1, 100), 1, zeros(1, 100));
+% sine = horzcat(zeros(1, 100), sin(2*pi*5e6.*linspace(0, 50e-6, 500)), zeros(1, 100));
+% square = horzcat(zeros(1, 100), ones(1, 100), zeros(1, 100));
+% test_sig = horzcat(preamble, impulse, zeros(1,40), sine, zeros(1, 40), square);
+% test_sig = upsample(test_sig, upsampling_rate);
 
 data = imread('images/shannon1440.bmp');
 dims = size(data);
-bits = reshape(data, dims(1)*dims(2), 1);
+bits = reshape(data, 1, dims(1)*dims(2));
 bits = bits .* 2 -1; %BPSK
-bits = horzcat(preamble, bits');
+data1 = bits(1:400);
+data2 = bits(401:800);
+data3 = bits(801:1200);
+data4 = bits(1201:min(length(bits), 1600));
+bits = horzcat(preamble, data1, frame_sync, data2, frame_sync, data3, frame_sync, data4);
 bits_up = upsample(bits, upsampling_rate);
-pulse = rcosdesign(0.5, 7, upsampling_rate, 'sqrt');
+pulse = rcosdesign(0.9, 30, upsampling_rate, 'sqrt');
+save('bits', 'bits');
 
-transmitsignal = conv(test_sig, pulse);
+transmitsignal = conv(bits_up, pulse);
 save("transmitsignal.mat", "transmitsignal");
 time = length(transmitsignal)/200; %In microseconds
 
@@ -66,35 +75,36 @@ yline(-20, 'red');
 yline(-40, 'black');
 
 %% Received
-load receivedsignal_test_cases.mat
-xt = receivedsignal;
-len = length(xt);
-
-figure(2)
-LargeFigure(gcf, 0.15); % Make figure large
-clf
-
-% Plot time domain signal
-subplot(2,1,1);
-hold on;
-plot([0:len-1]/200e6*1e6, real(xt))
-plot([0:len-1]/200e6*1e6, imag(xt))
-hold off;
-title('ybase(t)')
-xlabel('t in microseconds')
-ylabel('x(t)')
-legend("real", "imag");
-axis tight
-
-% Plot frequency domain signal
-subplot(2,1,2);
-plot([-len/2+1:len/2]*200/len, 20*log10(abs(fftshift(1/sqrt(len)*fft(xt)))))
-xlabel('DTFT frequency f in MHz')
-ylabel('abs(X(f)) in dB')
-xline(11, 'red');
-xline(-11, 'red');
-xline(15, 'black');
-xline(-15, 'black');
-yline(-20, 'red');
-yline(-40, 'black');
-axis tight
+% load receivedsignal.mat
+% xt = receivedsignal;
+% xt = xt(1:30000);
+% len = length(xt);
+% 
+% figure(2)
+% LargeFigure(gcf, 0.15); % Make figure large
+% clf
+% 
+% % Plot time domain signal
+% subplot(2,1,1);
+% hold on;
+% plot([0:len-1]/200e6*1e6, real(xt))
+% plot([0:len-1]/200e6*1e6, imag(xt))
+% hold off;
+% title('ybase(t)')
+% xlabel('t in microseconds')
+% ylabel('x(t)')
+% legend("real", "imag");
+% axis tight
+% 
+% % Plot frequency domain signal
+% subplot(2,1,2);
+% plot([-len/2+1:len/2]*200/len, 20*log10(abs(fftshift(1/sqrt(len)*fft(xt)))))
+% xlabel('DTFT frequency f in MHz')
+% ylabel('abs(X(f)) in dB')
+% xline(11, 'red');
+% xline(-11, 'red');
+% xline(15, 'black');
+% xline(-15, 'black');
+% yline(-20, 'red');
+% yline(-40, 'black');
+% axis tight
